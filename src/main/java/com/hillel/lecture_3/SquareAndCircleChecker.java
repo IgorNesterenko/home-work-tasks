2e2d513bf8decee255309237cca87fe60d8f6e89
Igor Nesterenko <nesterenko@segater.com>
2020-03-10 17:36:06 +0200

    Lesson 3 Hometask

--- src/main/java/com/hillel/lecture_3/SquareAndCircleChecker.java
+++ src/main/java/com/hillel/lecture_3/SquareAndCircleChecker.java
@@ -2,6 +2,9 @@ package com.hillel.lecture_3;
 
 import io.qameta.allure.Step;
 
+import static java.lang.Math.PI;
+import static java.lang.Math.sqrt;
+
 /**
  * Created by alpa on 10/22/19
  */
@@ -12,7 +15,15 @@ public class SquareAndCircleChecker {
 
 //        TODO implements result
         String result = "";
-
+       double squareSide = sqrt(squareArea);
+       double sircleDiametr = 2*sqrt(circleArea/PI);
+       if (squareSide >= sircleDiametr) {
+           System.out.println("The circle is in the square");
+           result = "The circle is in the square";
+       }else {
+           System.out.println("The circle is not in the square");
+           result = "The circle is not in the square";
+       }
         return result;
     }
 
@@ -21,7 +32,13 @@ public class SquareAndCircleChecker {
 
 //        TODO implements result
         String result = "";
-
+        if (squareArea < circleArea) {
+            System.out.println("The square is in the circle");
+            result = "The square is in the circle";
+        }else {
+            System.out.println("The square is not in the circle");
+            result = "The square is not in the circle";
+        }
         return result;
     }
 
