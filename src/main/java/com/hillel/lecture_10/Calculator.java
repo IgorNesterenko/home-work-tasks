e0378232e6343e3fecd6fb5441ca1bbdbe3a6731
Igor Nesterenko <nesterenko@segater.com>
2020-04-07 23:28:06 +0300

    lecture 10

---
+++ src/main/java/com/hillel/lecture_10/Calculator.java
@@ -0,0 +1,7 @@
+package com.hillel.lecture_10;
+
+/**
+ * Created by neste on 07.04.2020.
+ */
+public class Calculator {
+}
---
+++ src/test/java/com/hillel/lecture10/CalculatorTest.java
@@ -0,0 +1,7 @@
+package com.hillel.lecture10;
+
+/**
+ * Created by neste on 07.04.2020.
+ */
+public class CalculatorTest {
+}
